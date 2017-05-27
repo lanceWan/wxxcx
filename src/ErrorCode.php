7db48869aa761a9e8615f4caa6d9867b42159021
晚黎 <ibanya@126.com>
2017-05-27 13:10:24 +0800

    init

--- src/ErrorCode.php
+++ src/ErrorCode.php
@@ -1,7 +1,16 @@
 <?php
 namespace Iwanli\Wxxcx;
 
-
+/**
+ * error code 说明.
+ * <ul>
+ *    <li>-41001: encodingAesKey 非法</li>
+ *    <li>-41003: aes 解密失败</li>
+ *    <li>-41004: 解密后得到的buffer非法</li>
+ *    <li>-41005: base64加密失败</li>
+ *    <li>-41016: base64解密失败</li>
+ * </ul>
+ */
 class ErrorCode
 {
     public static $OK = 0;
